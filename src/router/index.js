324a17d8412e973c5e789efbb5f26b42e42236e4
sl106 <andrealin03@gmail.com>
2020-01-24 10:26:11 +0000

    added header component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Room from '../views/Room.vue'
+import Monitor from '../views/Monitor.vue'
+import League from '../views/League.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +14,19 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/room',
+    name: 'room',
+    component: Room
+  },
+  {
+    path: '/monitor',
+    name: 'monitor',
+    component: Monitor
+  },
+  {
+    path: '/league',
+    name: 'league',
+    component: League
   }
 ]
 
