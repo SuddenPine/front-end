2725a7d3376c707622678d5c744805d121c8ab66
sl106 <andrealin03@gmail.com>
2020-01-25 16:42:09 +0000

    added room layout

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,9 +1,44 @@
 <template>
-  <h1>This is the room page</h1>
+  <div class="modules">
+    <b-container fluid class="bv-example-row">
+      <b-row>
+        <b-col>
+          <Header />
+        </b-col>
+      </b-row>
+
+      <h1 style="height:50px"></h1>
+      <!-- <h1>{{ modules[0].alias }}</h1> -->
+      <b-row>
+        <b-col cols="3" v-for="room in roomlist" :key="room.alias">
+          <router-link :to="`/rooms/${room.alias}/`">
+            <b-card :img-src="room.img" img-alt="Image" img-top style="max-width: 20rem;">
+              <template v-slot:header>
+                <h4 class="mb-0">{{ room.title }}</h4>
+              </template>
+            </b-card>
+          </router-link>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
-export default {}
+import Header from '@/components/Header.vue'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'roomlist',
+  components: {
+    Header
+  },
+  computed: {
+    ...mapState({
+      roomlist: state => state.roomlist
+    })
+  }
+}
 </script>
 
 <style></style>
