1e08e11108466b06789ffc9478a3287a946f2fee
sl106 <andrealin03@gmail.com>
2020-01-25 16:41:56 +0000

    added baisc home cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,6 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />-->
-
     <b-container fluid class="bv-example-row">
       <b-row>
         <b-col>
@@ -11,7 +10,7 @@
       </b-row>
       <b-row>
         <b-col>
-          <Cards />
+          <HomeCards />
         </b-col>
       </b-row>
     </b-container>
@@ -22,13 +21,13 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import Header from '@/components/Header.vue'
-import Cards from '@/components/Cards.vue'
+import HomeCards from '@/components/HomeCards.vue'
 
 export default {
   name: 'home',
   components: {
     Header,
-    Cards
+    HomeCards
   }
 }
 </script>
