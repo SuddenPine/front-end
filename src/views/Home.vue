cec6e3491cc1108964198bdd7bceb93ad71986b5
sl106 <andrealin03@gmail.com>
2020-01-25 12:23:24 +0000

    added home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,19 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />-->
-    <Header />
+
+    <b-container fluid class="bv-example-row">
+      <b-row>
+        <b-col>
+          <Header />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <Cards />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -10,11 +22,13 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import Header from '@/components/Header.vue'
+import Cards from '@/components/Cards.vue'
 
 export default {
   name: 'home',
   components: {
-    Header
+    Header,
+    Cards
   }
 }
 </script>
