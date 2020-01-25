2129a8326438e1027e181540d70c07c390ffc822
sl106 <andrealin03@gmail.com>
2020-01-25 16:41:11 +0000

    added home cards

---
+++ src/components/HomeCards.vue
@@ -0,0 +1,79 @@
+<template>
+  <b-container class="bv-example-row">
+    <b-row>
+      <b-col>
+        <div>
+          <b-card
+            title="Lighting Overview"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 30rem;"
+            class="mb-2"
+          >
+            <b-card-text>On in 3 rooms</b-card-text>
+
+            <b-button href="#" variant="primary">More Detail</b-button>
+          </b-card>
+        </div>
+      </b-col>
+      <b-col>
+        <b-card
+          title="Heating Overview"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 30rem;"
+          class="mb-2"
+        >
+          <b-card-text>Comfort 20 °C</b-card-text>
+
+          <b-button href="#" variant="primary">More Detail</b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+
+    <b-row>
+      <b-col>
+        <div>
+          <b-card
+            title="Safety Alarms"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 30rem;"
+            class="mb-2"
+          >
+            <b-card-text>Everything OK</b-card-text>
+
+            <b-button href="#" variant="primary">More Detail</b-button>
+          </b-card>
+        </div>
+      </b-col>
+      <b-col>
+        <b-card
+          title="Doors & Windows"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 30rem;"
+          class="mb-2"
+        >
+          <b-card-text>All closed</b-card-text>
+
+          <b-button href="#" variant="primary">More Detail</b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
