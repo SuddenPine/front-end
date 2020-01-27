76cae77cca0d88b5056dc2c1338e9bf9941abed4
sl106 <andrealin03@gmail.com>
2020-01-27 09:09:43 +0000

    stared monitor page

--- src/views/Monitor.vue
+++ src/views/Monitor.vue
@@ -1,9 +1,29 @@
 <template>
-  <h1>This is the Monitor page</h1>
+  <div class="home">
+    <Header />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />-->
+    <b-container fluid class="bv-example-row">
+      <b-row align-h="center">
+        <b-col style="padding:1em">
+          <MonitorCards />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
-export default {}
-</script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import MonitorCards from '@/components/MonitorCards.vue'
 
-<style></style>
+export default {
+  name: 'home',
+  components: {
+    Header,
+    MonitorCards
+  }
+}
+</script>
